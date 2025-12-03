7b6626d361b907eca5039791d21e554806a573fe
Mauro <mauro8686@hotmail.com>
2025-12-03 11:39:50 -0300

    Modificacion de la ubicacion de los .jars

--- src/models/Gestor_App.java
+++ src/models/Gestor_App.java
@@ -3,6 +3,7 @@ package models;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -14,15 +15,17 @@ import javafx.stage.Stage;
 public class Gestor_App extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("MainView.fxml"));
-        Scene scene = new Scene(loader.load());
+        Parent root = FXMLLoader.load((getClass().getResource("/views/PrincipalView.fxml")));
+        Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.setTitle("Inventario de Productos");
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch(args);
+        //launch(args);
+        Application.launch();
+
     }
 }
 
